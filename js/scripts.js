03a92a5bbefc7f207f44c713fa12f57d629d62be
Student <example@example.com>
2019-10-30 14:04:14 -0700

    add broken functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 //back end//
 var vowels = ["a" , "e" , "i" , "o" , "u"];
-var consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z" , "qu"];
-var pigLatin = function(language) {
-  else {}
-}
+// var consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z" , "qu"];
+var pigLatin = function(input) {
+  var sentence = input.split(" ");
+  console.log(sentence)
+  // sentence.forEach(function(word){
+  //   word
+  }
 
-  console.log(consonants)
+
+  console.log(vowels)
 
 //front end//
 $(document).ready(function() {
@@ -13,7 +17,7 @@ $(document).ready(function() {
     event.preventDefault();
     var language = $("input#language");
     var result = pigLatin(language);
-
+    console.log("input#language");
     $("#result").show();
   });
 });
