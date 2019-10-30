21dea1f35bdbb15ab93b55c0247b6b60becf1e58
Student <example@example.com>
2019-10-30 11:57:06 -0700

    add back end function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+//back end//
+var vowels = ["a" , "e" , "i" , "o" , "u"];
+var consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z" , "qu"];
+var pigLatin = function(language) {
+  else {}
+}
+
+  console.log(consonants)
+
+//front end//
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
-    var language = $("input#")
-    var result = pigLatin(language)
+    var language = $("input#language");
+    var result = pigLatin(language);
+
+    $("#result").show();
   });
 });
