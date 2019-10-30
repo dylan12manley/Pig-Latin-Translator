7b5a74df8c54304b4ee6a22dcb88002d98223267
Student <example@example.com>
2019-10-30 15:28:47 -0700

    add additional, working js content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 //back end//
+function latinizer(word){
+  var pigWord = word;
+  if (/[^a-z]/i.test(word)) {
+    return pigWord;
+  }
+  else if (word.length === 1){
+    pigWord += "ay"
+
+  }
+  else if ((word.charAt(0).toLowerCase() === "a") || (word.charAt(0).toLowerCase() === "e") || (word.charAt(0).toLowerCase() === "i") || (word.charAt(0).toLowerCase() === "o") ||
+  (word.charAt(0).toLowerCase() === "u")) {
+    pigWord += "way"
+  }
+  
+}
 function toPigLatin(sentence){
   var sentenceArray = sentence.split(" ");
   var pigSentenceArray = sentenceArray.map(function(word){
     return latinizer(word);
-  })
+  });
 }
 
 
