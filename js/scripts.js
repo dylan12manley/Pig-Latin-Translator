5b2f44e51d0654309cba264a7f68afbc7e25088f
Student <example@example.com>
2019-10-30 16:42:34 -0700

    add additional js elements, good

--- js/scripts.js
+++ js/scripts.js
@@ -6,40 +6,58 @@ function latinizer(word){
   }
   else if (word.length === 1){
     pigWord += "ay"
-
+  console.log("one letter word");
   }
   else if ((word.charAt(0).toLowerCase() === "a") || (word.charAt(0).toLowerCase() === "e") || (word.charAt(0).toLowerCase() === "i") || (word.charAt(0).toLowerCase() === "o") ||
   (word.charAt(0).toLowerCase() === "u")) {
     pigWord += "way"
+    console.log("word starts with vowel");
   }
-  else if (!word.include("qu")){
+  else if (!word.includes("qu")){
     var firstVowelIndex = vowelIndexer(word)
     var pigWord = word.slice(firstVowelIndex) + word.slice(0, firstVowelIndex) + "ay"
+    console.log("word starting with consonant");
   }
   else {
     var firstVowelIndex = vowelIndexer(word)
     var pigWord = word.slice(firstVowelIndex + 1) + word.slice(0, firstVowelIndex + 1) + "ay"
+    console.log("word with qu");
+  }
+  return pigWord;
+}
+
+function vowelIndexer(string) {
+  for (var i = 1; i < string.length; i++) {
+    if ((string.charAt(i).toLowerCase() === "a") || (string.charAt(i).toLowerCase() === "e") || (string.charAt(i).toLowerCase() === "i") || (string.charAt(i).toLowerCase() === "o") ||
+    (string.charAt(i).toLowerCase() === "u")){
+      return i;
+      console.log(vowelIndexer);
+    }
   }
 }
+
+
 function toPigLatin(sentence){
   var sentenceArray = sentence.split(" ");
   var pigSentenceArray = sentenceArray.map(function(word){
     return latinizer(word);
+
   });
+  var pigSentence = pigSentenceArray.join(" ");
+  return pigSentence;
 }
 
-
-  console.log(vowels)
 
 //front end//
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
-    event.preventDefault();
     $("input#language").show();
     var userSentence = $("input#language").val();
     var pigLatinSentence = toPigLatin(userSentence);
     console.log("input#language");
-    $("#result").text(pigLatinSentence);
+    $("#result").show();
+    $("#translation").text(pigLatinSentence);
+    event.preventDefault();
   });
 });
 
