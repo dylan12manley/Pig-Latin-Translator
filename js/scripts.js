825076efe9f2620165843bab13efbdd0a6fd86ec
Student <example@example.com>
2019-10-30 14:41:51 -0700

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 //back end//
-var vowels = ["a" , "e" , "i" , "o" , "u"];
-// var consonants = ["b" , "c" , "d" , "f" , "g" , "h" , "j" , "k" , "l" , "m" , "n" , "p" , "q" , "r" , "s" , "t" , "v" , "w" , "x" , "y" , "z" , "qu"];
-var pigLatin = function(input) {
+var vowels = ["a" , "e" , "i" , "o" , "u" , "A" , "E" , "I" , "O" , "U"];
+
+var pigSentence = function(input) {
   var sentence = input.split(" ");
-  console.log(sentence)
-  // sentence.forEach(function(word){
-  //   word
+  // console.log(sentence)
+  newSentence = sentence.map(function(word){
+    return pigWord(word);
+  });
+  return newSentence.join(" ");
+
+};
+
+var pigWord = function(word){
+  if (word[0] === "y" || word[0] === "Y") {
+    return word.slice1
+
   }
+  else {
+  return (pigWord("error"))}
+}
 
 
   console.log(vowels)
@@ -15,8 +27,8 @@ var pigLatin = function(input) {
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
-    var language = $("input#language");
-    var result = pigLatin(language);
+    var language = $("input#language").val();
+    var result = pigSentence(language);
     console.log("input#language");
     $("#result").show();
   });
